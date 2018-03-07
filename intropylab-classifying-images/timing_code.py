f82059d086ffa98ffbe1af82ea289ea18ed27841
j-staab <jennifer.staab@udacity.com>
2018-03-06 19:21:16 -0500

    Adding timing_code.py to repository.

---
+++ intropylab-classifying-images/timing_code.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND/intropylab-classifying-images/timing_code.py
+#                                                                             
+# PROGRAMMER: Jennifer S.                                                    
+# DATE CREATED: 02/16/2018                                  
+# REVISED DATE:             <=(Date Revised - if any)                         
+# PURPOSE: Timing Code example from AIPND 
+#
+#   Example call:
+#    python timing_code.py 
+##
+
+# Imports time() and sleep() functions from time module
+from time import time, sleep
+
+
+# Main program function defined below
+def main():
+    # Sets start time 
+    start_time = time()
+    
+    # Replace sleep(75) below with code you want to time
+    sleep(75)
+    
+    # Sets end time
+    end_time = time()
+
+    # Computes overall run time in seconds
+    tot_time = end_time - start_time
+    
+    # Prints overall run time in seconds
+    print("\nTotal Elapsed Run time:", tot_time, "in seconds.")
+           
+    # Prints overall run time in format hh:mm:ss
+    print("\nTotal Elapsed Run time:", str( int( (tot_time / 3600) ) ) + ":" + 
+          str( int(  ( (tot_time % 3600) / 60 )  ) ) + ":" + 
+          str( int( ( (tot_time % 3600) % 60 ) ) ) )
+                       
+# Call to main function to run the program
+if __name__ == "__main__":
+    main()
