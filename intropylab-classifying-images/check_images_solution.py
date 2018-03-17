6759a6c4fecb74e3880c29a88a0f79869581b8aa
j-staab <jennifer.staab@udacity.com>
2018-03-16 21:02:51 -0400

    Final comment fixes to both prior to upload to workspace.

--- intropylab-classifying-images/check_images_solution.py
+++ intropylab-classifying-images/check_images_solution.py
@@ -13,7 +13,7 @@
 #      python check_images.py --dir <directory with images> --arch <model>
 #             --dogfile <file that contains dognames>
 #   Example call:
-#    python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt
+#    python check_images_solution.py --dir pet_images/ --arch vgg --dogfile dognames.txt
 ##
 
 # Imports python modules
