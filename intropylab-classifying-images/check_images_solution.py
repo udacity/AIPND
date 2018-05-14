f2b777c6cfa1c815d0803b27dc39b92f436e7e92
j-staab <jennifer.staab@udacity.com>
2018-05-14 12:01:25 -0400

    Added import statement to check_images_solution to allow students to use the print functions in print_functions_for_lab_checks.py to more easily check their code AND added function calls to those print functions so that students would know how to use them to check their code.

--- intropylab-classifying-images/check_images_solution.py
+++ intropylab-classifying-images/check_images_solution.py
@@ -5,6 +5,7 @@
 # PROGRAMMER: Jennifer S.
 # DATE CREATED: 01/30/2018                                  
 # REVISED DATE: 02/27/2018  - reduce scope of program
+# REVISED DATE: 05/14/2018 - added printing functions for checking the lab
 # PURPOSE: Check images & report results: read them in, predict their
 #          content (classifier), compare prediction to actual value labels
 #          and output results
@@ -24,6 +25,9 @@ from os import listdir
 # Imports classifier function for using CNN to classify images 
 from classifier import classifier 
 
+# Imports print functions that check the lab
+from print_functions_for_lab_checks import *
+
 # Main program function defined below
 def main():
     # Measures total program runtime by collecting start time
@@ -31,21 +35,41 @@ def main():
     
     # Creates & retrieves Command Line Arugments
     in_arg = get_input_args()
+
+    # Function that checks command line arguments using in_arg 
+    check_command_line_arguments(in_arg)
+
     
     # Creates Pet Image Labels by creating a dictionary 
     answers_dic = get_pet_labels(in_arg.dir)
 
+    # Function that checks Pet Images Dictionary- answers_dic    
+    check_creating_pet_image_labels(answers_dic)
+
+    
     # Creates Classifier Labels with classifier function, Compares Labels, 
     # and creates a results dictionary 
     result_dic = classify_images(in_arg.dir, answers_dic, in_arg.arch)
+
+    # Function that checks Results Dictionary - result_dic    
+    check_classifying_images(result_dic)    
+
     
     # Adjusts the results dictionary to determine if classifier correctly 
     # classified images as 'a dog' or 'not a dog'. This demonstrates if 
     # model can correctly classify dog images as dogs (regardless of breed)
     adjust_results4_isadog(result_dic, in_arg.dogfile)
+
+    # Function that checks Results Dictionary for is-a-dog adjustment- result_dic  
+    check_classifying_labels_as_dogs(result_dic)
+
     
     # Calculates results of run and puts statistics in results_stats_dic
     results_stats_dic = calculates_results_stats(result_dic)
+
+    # Function that checks Results Stats Dictionary - results_stats_dic  
+    check_calculating_results(result_dic, results_stats_dic)
+
 
     # Prints summary results, incorrect classifications of dogs
     # and breeds if requested
